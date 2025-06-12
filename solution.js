3a72c64310cced7e9bc58e97f324fbcdfb25943b
Nafiul-islam <157912936+NafiulAntu@users.noreply.github.com>
2025-06-12 16:36:12 +0600

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,9 +1,4 @@
-import express from "express";
-import bodyParser from "body-parser";
-import axios from "axios";
 
-const app = express();
-const port = 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
